5adf714584776a1feda74c0d07f0eedc2596a54f
Violet <violtat@sina.com>
2019-06-06 21:13:16 +0800

    hlp end

--- src/components/RecordMa/RechargeRecord.vue
+++ src/components/RecordMa/RechargeRecord.vue
@@ -1,100 +1,114 @@
 <template>
   <div id="recharge_app">
-    <div class="title">
-      <h2>充值记录</h2>
-    </div>
-    <div id="nav">
-      <div class="one">
-        <!-- 搜索框 -->
-        <Search :searchOpt="searchOpt"/>
-        <!-- 导出按钮 el-icon-download -->
-        <!-- <el-row class="export" style="margin-right: 0; margin-top: 15px; display: inline-block;">
-          <el-button plain @click="exportExcel">导出</el-button>
-        </el-row> -->
+    <Title :navArr="navArr"/>
+    <div class="app_content">
+      <div id="nav">
+        <div class="one">
+          <!-- 搜索框 -->
+          <!-- <Search :searchOpt="searchOpt"/> -->
+          <div style="display: inline-block;">
+            <!-- 搜索选项 -->
+            <el-input placeholder="请输入内容" v-model="input5" class="input-with-select">
+              <el-select v-model="searchSel" slot="prepend" placeholder="请选择">
+                <el-option
+                  v-for="item in searchOpt"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                ></el-option>
+              </el-select>
+              <el-button @click="getSearchList" slot="append" icon="el-icon-search"></el-button>
+            </el-input>
+          </div>
+        </div>
+        
+        <div class="two">
+          <!-- 选择充值方式 -->
+          <Mode :modeOpt="modeOpt"/>
+          <!-- 选择状态 -->
+          <Status :statusOpt="statusOpt"/>
+          <!-- 日期选择器 -->
+          <DatePicke/>
+          <!-- 导出 -->
+          <div class="export">
+            <el-select v-model="value" placeholder="批量导出" class="exportWidth">
+              <el-option @click="exportExcel" label="导出全部" value="1"></el-option>
+              <el-option label="导出选中" value="2"></el-option>
+            </el-select>
+          </div>
+        </div>
       </div>
-      <div class="two">
-        <!-- 选择充值方式 -->
-        <p>充值方式筛选：</p>
-        <Mode :modeOpt="modeOpt"/>
-        <!-- 选择状态 -->
-        <p>状态筛选：</p>
-        <Status :statusOpt="statusOpt"/>
-        <!-- 日期选择器 -->
-        <p>日期筛选：</p>
-        <DatePicke/>
-        <!-- 导出 -->
-        <Export/>
-      </div>
-    </div>
 
-    <!-- 表格 -->
-    <div class="wrapper" style="padding-top: 30px;" id="RechargeRecord">
-      <div class="wrapper-content">
-        <!-- ref="multipleTable"
+      <!-- 表格 -->
+      <div class="wrapper" style="padding-top: 30px;" id="RechargeRecord">
+        <div class="wrapper-content">
+          <!-- ref="multipleTable"
           tooltip-effect="dark" 
-        show-summary="true"-->
-        <el-table
-          :data="tableData.slice((currentPage-1)*pagesize,currentPage*pagesize)"
-          stripe
-          :header-cell-style="{color:'#333',backgroundColor:'#EBEEF5'}"
-          @selection-change="handleSelectionChange"
-          style="width: 100%"
-        >
-          <el-table-column type="selection"></el-table-column>
-          <el-table-column prop="loan_id" label="充值单号"></el-table-column>
-          <el-table-column prop="loan_money" label="用户手机"></el-table-column>
-          <!-- <el-table-column prop="loan_money" label="真实姓名"></el-table-column> -->
-          <!-- <el-table-column prop="loan_ deadline" label="用户来源"></el-table-column> -->
-          <!-- <el-table-column prop="loan_ deadline" label="应用来源"></el-table-column> -->
-          <el-table-column prop="loan_money" label="充值金额"></el-table-column>
-          <el-table-column prop="loan_money" label="到账金额"></el-table-column>
-          <el-table-column prop="loan_money" label="手续费"></el-table-column>
-          <el-table-column prop="loan_money" label="充值方式"></el-table-column>
-          <el-table-column prop="loan_money" label="交易流水号"></el-table-column>
-          <el-table-column label="订单时间">
-            <template slot-scope="scope">
-              <p>{{ scope.row.loan_date | dateFormat }}</p>
-            </template>
-          </el-table-column>
-          <el-table-column label="到账时间">
-            <template slot-scope="scope">
-              <p>{{ scope.row.loan_date | dateFormat }}</p>
-            </template>
-          </el-table-column>
-          <el-table-column prop="status" label="状态"></el-table-column>
-          <el-table-column prop="do" label="操作" show-overflow-tooltip>
-            <template slot-scope="scope">
-              <el-button
-                type="primary"
-                icon="el-icon-view"
-                size="mini"
-                @click="handleView(scope.$index, scope.row)"
-              >查看</el-button>
-            </template>
-          </el-table-column>
-        </el-table>
+          show-summary="true"-->
+          <el-table
+            :data="tableData.slice((currentPage-1)*pagesize,currentPage*pagesize)"
+            stripe
+            :header-cell-style="{color:'#333',backgroundColor:'#EBEEF5'}"
+            @selection-change="handleSelectionChange"
+            style="width: 100%"
+          >
+            <el-table-column type="selection"></el-table-column>
+            <el-table-column prop="loan_id" label="充值单号"></el-table-column>
+            <!-- <el-table-column prop="loan_money" label="用户手机"></el-table-column> -->
+            <el-table-column prop="loan_money" label="真实姓名"></el-table-column>
+            <!-- <el-table-column prop="loan_ deadline" label="用户来源"></el-table-column> -->
+            <!-- <el-table-column prop="loan_ deadline" label="应用来源"></el-table-column> -->
+            <el-table-column prop="loan_money" label="充值金额"></el-table-column>
+            <el-table-column prop="loan_money" label="到账金额"></el-table-column>
+            <el-table-column prop="loan_money" label="手续费"></el-table-column>
+            <el-table-column prop="loan_money" label="充值方式"></el-table-column>
+            <!-- <el-table-column prop="loan_money" label="交易流水号"></el-table-column> -->
+            <el-table-column label="订单时间" width="160px">
+              <template slot-scope="scope">
+                <p>{{ scope.row.loan_date | dateFormat }}</p>
+              </template>
+            </el-table-column>
+            <el-table-column label="到账时间" width="160px">
+              <template slot-scope="scope">
+                <p>{{ scope.row.loan_date | dateFormat }}</p>
+              </template>
+            </el-table-column>
+            <el-table-column prop="status" label="状态"></el-table-column>
+            <el-table-column prop="do" label="操作" show-overflow-tooltip>
+              <template slot-scope="scope">
+                <el-button
+                  type="primary "
+                  icon="el-icon-view"
+                  size="mini"
+                  @click="handleView(scope.$index, scope.row)"
+                >查看</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </div>
       </div>
-    </div>
 
-    <!-- 分页 -->
-    <Pagination
-      :total="total"
-      :pagesize="pagesize"
-      :currentPage="currentPage"
-      :current_change="current_change"
-      :handleSizeChange="handleSizeChange"
-    />
+      <!-- 分页 -->
+      <Pagination
+        :total="total"
+        :pagesize="pagesize"
+        :currentPage="currentPage"
+        :current_change="current_change"
+        :handleSizeChange="handleSizeChange"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import Title from "./../commonComponents/headerTitle";
 import FileSaver from "file-saver";
 import XLSX from "xlsx";
 
-import Search from "./Subassembly/Search.vue";
+// import Search from "./Subassembly/Search.vue";
 import Mode from "./Subassembly/Mode";
 import Status from "./Subassembly/Status";
-import Export from "./Subassembly/Export";
+// import Export from "./Subassembly/Export";
 // import Atable from "./Subassembly/Atable";
 import Pagination from "./Subassembly/Pagination";
 import DatePicke from "./Subassembly/DatePicke";
@@ -102,21 +116,25 @@ import DatePicke from "./Subassembly/DatePicke";
 export default {
   name: "RechargeRecord",
   components: {
-    Search,
+    // Search,
     Mode,
     Status,
-    Export,
+    // Export,
     Pagination,
-    DatePicke
+    DatePicke,
+    Title
     // Atable
   },
 
   data() {
     return {
+      input5: "",
+      searchSel: "",
       tableData: [],
       currentPage: 1,
       pagesize: 5,
       total: 0,
+      value: "",
       tableDatas: [
         {
           reId: "2017040031",
@@ -150,15 +168,27 @@ export default {
         { value: 2, label: "充值成功" },
         { value: 3, label: "充值失败" },
         { value: 4, label: "充值处理中" }
-      ]
+      ],
+      navArr: ["资金管理", "充值记录"]
     };
   },
 
   methods: {
+    // 获取搜索列表
+    getSearchList(pageSize,currentPage){
+      this.Axios.post("",{}).then(res => {
+        console.log(res);
+        this.tableData = res.data.datas.data;
+        this.total = this.tableData.length;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    },
     // 查看
     handleView(index, row) {
-        console.log(index, row);
-      },
+      console.log(index, row);
+    },
     // 全选
     handleSelectionChange(val) {
       this.multipleSelection = val;
@@ -221,26 +251,30 @@ export default {
   padding: 0;
   position: relative;
 }
+#recharge_app > .app_content {
+  margin: 20px;
+}
 #recharge_app > #nav {
   width: 100%;
-}
-.title {
-  width: 100%;
-  height: 40px;
-  background-color: #006d75;
-}
-h2 {
-  color: #fff;
-  margin-left: 10px;
-  line-height: 40px;
 }
 #nav .one,
 #nav .two {
   position: relative;
 }
+.el-select .el-input {
+  width: 130px;
+}
+.input-with-select .el-input-group__prepend {
+  background-color: #fff;
+}
+.input-with-select > .el-input__inner {
+  width: 160px;
+}
 .export {
   position: absolute;
   top: 0;
   right: 0;
+  margin-top: 15px;
+  display: inline-block;
 }
 </style>
