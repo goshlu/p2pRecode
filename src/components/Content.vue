991cc003991155771b4e523afb139facffe0873f
Qiliin <joon_e@126.com>
2019-06-06 16:24:36 +0800

    QQ

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="Content">
-    <Nav></Nav>
+    <div class="navBox">
+      <Nav></Nav>
+    </div>
     <div class="ContentBox">
       <div>
         <router-view></router-view>
@@ -25,9 +27,32 @@ export default {
 </script>
 
 <style lang="stylus">
+  .navBox
+    // position: fixed
+    width: 100%
+    // top: 0
+    // left: 0
+    // z-index 100
   .ContentBox
-    margin 10px
+    // margin-top: 10px
+    height calc(100vh - 121px)
+    overflow auto
+    background-color: #f2f2f2
+    /* 设置滚动条的样式 */
+    &::-webkit-scrollbar 
+      width: 2px;
+
+    /* 滚动槽 */
+    &::-webkit-scrollbar-track 
+      -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.6);
+      border-radius: 2px;
+
+    /* 滚动条滑块 */
+    &::-webkit-scrollbar-thumb 
+      border-radius: 2px;
+      background: rgba(0, 0, 0, 0.1);
+      -webkit-box-shadow: inset 0 0 6 pxrgba(0, 0, 0, 0.6);
     >div
-      padding 10px
+      margin 10px
       background-color #fff
 </style>
