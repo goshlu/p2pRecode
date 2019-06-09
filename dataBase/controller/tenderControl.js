5fd21e6366f4f6edff52f0b509f9e13b88181ece
joetolearn <302507446@qq.com>
2019-06-09 20:49:47 +0800

    zcb1.1 6-9

--- dataBase/controller/tenderControl.js
+++ dataBase/controller/tenderControl.js
@@ -1,26 +1,25 @@
-let tenderModel=require("../modal/tenderModel");
+let tenderModel = require("../modal/tenderModel");
 
 //查询所有借款标
-function getTenderAll(req,res,next){
-	// console.log(req.query);
-	let id=req.query.id;
-   let account_type=req.query.account_type;
-   let type=req.query.type;
-   // console.log(usertype);
-     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  tenderModel.getTenderAll(id,account_type,type,function(err,data){
-       if(!err){
-               res.send(data);
-			   // console.log("返回成功");
-			   next();
-       }else{
-           res.send("101,联系管理员");
-       }
-   });
+function getTenderAll(req, res, next) {
+  console.log(req.query);
+  let name = req.query.name;
+  let phone = req.query.phone;
+  let status = req.query.status;
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  tenderModel.getTenderAll(req.query, function (err, data) {
+    if (!err) {
+      res.send(data);
+      // console.log("返回成功");
+      next();
+    } else {
+      res.send("101,联系管理员");
+    }
+  });
 }
 
-module.exports={
+module.exports = {
   getTenderAll
 }
--- dataBase/modal/tenderModel.js
+++ dataBase/modal/tenderModel.js
@@ -1,33 +1,23 @@
 let pool=require("./pool");
 
 //所有借款标
-function getTenderAll(id,account_type,type,callback) {
+function getTenderAll({name, phone, status},callback) {
 	// console.log("进入数据库")
 	let arr=[];
     let sql = `select * from tender where 1=1`;
-	 if(id){
-	    id="%"+id+"%";
-	    sql+=" and id like ? "
-	    arr.push(id);
+	 if(name){
+     name="%"+name+"%";
+	    sql+=" and name like ? "
+	    arr.push(name);
 	}
-	 if(account_type){
-	    if(account_type==0){
-	   			 account_type=="";
-	   }else{
-	   			   account_type="%"+account_type+"%";
-	   			 sql+=" and account_type like ? "
-	   			 arr.push(account_type);
-	   }
+	 if(phone){
+     phone="%"+phone+"%";
+     sql+=" and id like ? "
+     arr.push(phone);
 	}
-	 if(type){
-		 if(type==0){
-			 type=="";
-		 }else{
-			   type="%"+type+"%";
-			 sql+=" and type like ? "
-			 arr.push(type);
-		 }
-	  
+	 if(status){
+     sql+=" and status = ? "
+     arr.push(status);
 		// console.log(usertype);
 	}
     // where staffUser =? and pwd =?`;
