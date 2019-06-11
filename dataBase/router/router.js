a9b81f246bb3a6a18a827693b135910b10b254c2
joetolearn <302507446@qq.com>
2019-06-11 09:10:01 +0800

    zcb1 6-11

--- dataBase/router/router.js
+++ dataBase/router/router.js
@@ -6,6 +6,9 @@ let tenderControl=require("../controller/tenderControl");
 //借贷管理
 let dictionaryControl=require("../controller/dictionaryControl");
 
+//部门员工
+let jsonDataControl=require("../controller/jsonDataControl");
+
 mRouter.all("/usercapital",control.usercapital);
 mRouter.all("/capitaljournal",control.capitaljournal);
 mRouter.all("/platformfunds",control.platformfunds);
@@ -17,11 +20,23 @@ mRouter.all("/tenderall",control.tenderall);
 mRouter.all("/getBorrows",control.getBorrows);
 //获取所有借款标列表 （搜索传：name, phone, status, page, limit）
 mRouter.all("/getTenderAll",tenderControl.getTenderAll);
-//获取所有借款标列表addTender
+//添加借款标
 mRouter.all("/addTender",tenderControl.addTender);
+//修改、编辑借款标
+mRouter.all("/editTender",tenderControl.editTender);
+//删除，下架借款标
+mRouter.all("/deleteTender",tenderControl.deleteTender);
+//审核标
+mRouter.all("/auditTender",tenderControl.auditTender);
 
 //字典表管理
 mRouter.all("/getDictionaries",dictionaryControl.getAll);
+
+//部门、员工
+mRouter.all("/getEmpolyees",jsonDataControl.getEmpolyees);
+mRouter.all("/getDeparts",jsonDataControl.getDeparts);
+mRouter.all("/getAll",jsonDataControl.getAll);
+mRouter.all("/addEmployee",jsonDataControl.addEmployee);
 
 // app.get("/Student",function(){});
 module.exports=mRouter;
